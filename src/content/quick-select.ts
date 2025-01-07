45d709b7cd1f39e0d8ab5cbfbc325ef8e9908a4d
Jan Bulling <jan.greenengage@gmail.com>
2025-01-07 13:38:24 +0100

    updated links and images

--- src/content/quick-select.ts
+++ src/content/quick-select.ts
@@ -3,21 +3,42 @@ export const quickSelect = [
     name: "Moodle",
     href: "https://moodle.uni-ulm.de",
     description: "Lernplatform",
+    image: "/img/moodle.png",
   },
   {
     name: "CampusOnline",
-    href: "https://moodle.uni-ulm.de",
+    href: "https://campusonline.uni-ulm.de/",
     description: "Notenübersicht",
+    image: "/img/uni-logo.png",
+    darkImageInvert: true,
+  },
+  {
+    name: "Sogo",
+    href: "https://sogo.uni-ulm.de/",
+    description: "Emails",
+    image: "/img/sogo.png",
   },
   {
     name: "Campus 4",
-    href: "https://moodle.uni-ulm.de",
+    href: "https://campus4.uni-ulm.de/",
     description: "Studienbescheinigungen",
   },
   {
     name: "Mensa",
     href: "https://mensa.jan-bulling.com",
-    description: "Tägliches Essensangebot an der Uni Ulm",
+    description: "Tägliches Essensangebot",
+    image: "/img/studierendenwerk.jpg",
+  },
+  {
+    name: "CloudStore",
+    href: "https://cloudstore.uni-ulm.de",
+    description: "Cloud Speicherplatz",
+    image: "/img/cloudstore.webp",
+  },
+  {
+    name: "Print",
+    href: "https://print.uni-ulm.de",
+    description: "Drucken im Uninetz",
   },
 ];
 
