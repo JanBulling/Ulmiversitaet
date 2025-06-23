f321141b383aa965a873ec4968d0e5f03ba8cb95
Stefan Rau <167868943+raustefan@users.noreply.github.com>
2025-06-23 08:31:24 +0200

    Update announcements.ts

--- src/content/announcements.ts
+++ src/content/announcements.ts
@@ -1,19 +1,19 @@
 export const announcements: Announcement[] = [
+  {
+    title: "Jan ist in wieder da!",
+    description:
+      "Jan ist aus Uppsala zurückgekehrt und wieder in Ulm!",
+  },
+
+
+        
         {
     title: "Sommersemester!",
     description:
       "Wir wünschen euch viel Spaß und Lernerfolg im Sommersemester",
   },
-    {
-    title: "Viva La PhyMa",
-    description:
-      "Am 05.06. findet die beste Uniparty des Jahres im Forum statt! Der Vorverkauf startet am 22.05. um 12 Uhr.",
-  },
-  {
-    title: "Jan ist in Uppsala",
-    description:
-      "Ab dem 16. Januar 2025 ist Jan leider nicht mehr in Ulm. Aber keine Sorge: Er kommt wieder zurück!",
-  },
+
+
 
 ];
 
